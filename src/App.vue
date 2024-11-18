0b5281694547e7a5ac809f86d389b97bd029f78f
abdullah <88263280+abdurrahmanabdullah@users.noreply.github.com>
2024-11-18 21:22:51 +0600

    nested function work fine

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,126 @@
 <template>
-  <div id="app">
-    <TodoApp msg="Welcome to Your Vue.js Todo App" />
+  <div class="container">
+    <h2 class="text-center mt-5">My Vue Todo App</h2>
+
+    <div class="input-group mb-3">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Enter New Task here"
+        v-model="newTask"
+        @keyup.enter="addTask"
+      />
+      <button class="btn btn-primary" @click="addTask">Add Task</button>
+    </div>
+
+    <div>
+      <ul>
+        <task-item
+          v-for="task in tasks"
+          :key="task.id"
+          :task="task"
+          @delete-task="deleteTask"
+          @add-subtask="addSubtask"
+        ></task-item>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import TodoApp from "./components/TodoApp.vue";
+import TaskItem from "./components/TaskDisplay.vue";
+import { v4 as uuidv4 } from "uuid";
 
 export default {
   components: {
-    TodoApp,
+    TaskItem,
+  },
+  data() {
+    return {
+      tasks: [],
+      newTask: "",
+    };
+  },
+  created() {
+    const savedTasks = localStorage.getItem("tasks");
+    if (savedTasks) {
+      this.tasks = JSON.parse(savedTasks);
+      console.log("Loaded tasks from localStorage:", this.tasks);
+    }
+  },
+  methods: {
+    saveTasks() {
+      console.log("Saving tasks:", JSON.stringify(this.tasks, null, 2));
+      localStorage.setItem("tasks", JSON.stringify(this.tasks));
+    },
+    logHierarchy() {
+      console.log(
+        "Current Task Hierarchy:",
+        JSON.stringify(this.tasks, null, 2)
+      );
+    },
+    addTask() {
+      if (this.newTask.trim()) {
+        this.tasks.push({
+          id: uuidv4(),
+          name: this.newTask,
+          subtasks: [],
+        });
+        this.newTask = "";
+        this.saveTasks();
+        this.logHierarchy();
+      }
+    },
+    addSubtask(parentId) {
+      const subtaskName = prompt("Enter subtask name:");
+      if (subtaskName) {
+        const newSubtask = {
+          id: uuidv4(),
+          name: subtaskName,
+          subtasks: [],
+        };
+        this.addSubtaskToParent(this.tasks, parentId, newSubtask);
+        this.saveTasks();
+        this.logHierarchy();
+      }
+    },
+    addSubtaskToParent(tasks, parentId, newSubtask) {
+      for (const task of tasks) {
+        if (task.id === parentId) {
+          task.subtasks.push(newSubtask);
+          return;
+        }
+        if (task.subtasks.length) {
+          this.addSubtaskToParent(task.subtasks, parentId, newSubtask);
+        }
+      }
+    },
+    deleteTask(taskId) {
+      this.deleteTaskById(this.tasks, taskId);
+      this.saveTasks();
+      this.logHierarchy();
+    },
+    deleteTaskById(tasks, taskId) {
+      const index = tasks.findIndex((task) => task.id === taskId);
+      if (index !== -1) {
+        tasks.splice(index, 1);
+        return true;
+      }
+      for (const task of tasks) {
+        if (task.subtasks.length) {
+          const found = this.deleteTaskById(task.subtasks, taskId);
+          if (found) return true;
+        }
+      }
+      return false;
+    },
   },
 };
 </script>
+
+<style scoped>
+.container {
+  max-width: 600px;
+  margin: auto;
+}
+</style>
---
+++ src/components/TaskDisplay.vue
@@ -0,0 +1,43 @@
+<template>
+  <li>
+    <div>
+      {{ task.name }}
+      <button
+        class="btn btn-danger btn-sm my-2"
+        @click="$emit('delete-task', task.id)"
+      >
+        Delete
+      </button>
+      <button
+        class="btn btn-secondary btn-sm"
+        @click="$emit('add-subtask', task.id)"
+      >
+        Add Subtask
+      </button>
+    </div>
+    <ul v-if="task.subtasks.length">
+      <task-item
+        v-for="subtask in task.subtasks"
+        :key="subtask.id"
+        :task="subtask"
+        @delete-task="$emit('delete-task', $event)"
+        @add-subtask="$emit('add-subtask', $event)"
+      ></task-item>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "TaskItem",
+  props: {
+    task: Object,
+  },
+};
+</script>
+
+<style scoped>
+li {
+  margin: 10px 0;
+}
+</style>
